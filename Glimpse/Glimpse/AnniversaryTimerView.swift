07b89b68ed37e521e30993bfaae84809160eee34
Andera Singgih Pratama <anderasinggih1@gmail.com>
2026-05-17 00:24:06 +0700

    Replaced leftover glassmorphic calls with native ultraThinMaterial in AnniversaryTimerView

--- Glimpse/Glimpse/AnniversaryTimerView.swift
+++ Glimpse/Glimpse/AnniversaryTimerView.swift
@@ -20,7 +20,8 @@ struct AnniversaryTimerView: View {
         }
         .padding(.vertical, 12)
         .padding(.horizontal, 24)
-        .glassmorphic()
+        .background(.ultraThinMaterial)
+        .clipShape(RoundedRectangle(cornerRadius: 24, style: .continuous))
         .onAppear(perform: updateTimer)
         .onReceive(timer) { _ in
             withAnimation(.spring(response: 0.3, dampingFraction: 0.7)) {
